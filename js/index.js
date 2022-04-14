c92682c8a3b8dcb1bbf029e268918b63f331d093
kiannaamaya <kianna.am.hendricks@gmail.com>
2022-04-14 18:47:56 -0400

    Initial commit

---
+++ js/index.js
@@ -0,0 +1,103 @@
+setTimeout(() => {
+    document.body.classList.remove("preload");
+  }, 500);
+  
+
+
+const buttonOptions = { //dictionary object with key/value pairing
+    "rock": "/images/Rock.png",
+    "paper": "/images/Paper.png",
+    "scissors": "/images/Scissors.png"
+}
+
+let SCORE = 0;
+
+
+const playerChoice = (button) => {
+    //hide current page
+    let buttonChoices = document.querySelector(".buttonChoices");
+    buttonChoices.style.display = "none"; //this will hide the 3 buttons 
+
+    let comparison = document.querySelector(".comparison");
+    comparison.style.display = "flex"; //this will show the comparision div when a button is clicked by the player
+
+    //set the player choice
+    //when player picks a button, it displays the comparison screen with the currently picked button
+    document.getElementById("playerPickImage").src = buttonOptions[button]; //go to dictionary, find key and "unlocks" the value paired with it 
+
+    let houseButton = houseChoice();
+    results(button, houseButton);
+}
+
+const houseChoice = () => {
+    let buttonChoices = ["rock", "paper", "scissors"];
+    let houseButton = buttonChoices[Math.floor(Math.random()*3)];
+
+    document.getElementById("housePickImage").src = buttonOptions[houseButton];
+
+    return houseButton;
+}
+
+const results = (playerButton, houseButton) => {
+
+    if(playerButton == "paper" && houseButton == "scissors") {
+        setDecision("YOU LOSE!");
+    } 
+    if(playerButton == "paper" && houseButton == "rock") {
+        setDecision("YOU WIN!");
+        setScore(SCORE + 1);
+    } 
+    if (playerButton == "paper" && houseButton == "paper") {
+        setDecision("It's a tie!");
+    }
+      
+    if (playerButton == "rock" && houseButton == "scissors") {
+        setDecision("YOU WIN!");
+        setScore(SCORE + 1);
+    }
+      
+    if (playerButton == "rock" && houseButton == "paper") {
+        setDecision("YOU LOSE!");
+    }
+      
+    if (playerButton == "rock" && houseButton == "rock") {
+        setDecision("It's a tie!");
+    }
+      
+    if (playerButton == "scissors" && houseButton == "scissors") {
+        setDecision("It's a tie!");
+    }
+      
+    if (playerButton == "scissors" && houseButton == "rock") {
+        setDecision("YOU LOSE!");
+    }
+      
+    if (playerButton == "scissors" && houseButton == "paper") {
+        setDecision("YOU WIN!");
+        setScore(SCORE + 1);
+    }
+
+}
+
+
+const restartGame = () => {
+
+    let buttonChoices = document.querySelector(".buttonChoices");
+    buttonChoices.style.display = "flex"; //this will hide the 3 buttons 
+
+    let comparison = document.querySelector(".comparison");
+    comparison.style.display = "none"; //this will show the comparision div when a button is clicked by the player
+
+
+}
+const setDecision = (decision) => {
+    document.querySelector(".resultText h1").innerText = decision; //accessing the html element, h1, and changing it based on the game results that is based in if statements
+}
+
+
+const setScore = (score) => {
+    SCORE = score; //saving score so that it will continue increasing
+    document.querySelector(".score h1").innerText = score; //access h1 for score and changes it based on game results from if statements above
+}
+
+
---
+++ plan.md
@@ -0,0 +1,9 @@
+My Steps to Completing This Project
+
+1. Create the banner [Rock, Paper, Scissors and Score card]
+2. Create the game buttons
+3. Create choice comparison screen with player button on left, computer button on right, and "play again" square button in the middle
+3. Create :onclick event for buttons
+4. After onclick event: hide initial screen
+5. Decide the computer, "The House", choice (a random choice)
+6. Decide a winner
